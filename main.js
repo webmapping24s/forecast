92f64dcb61e67338970d1f3deb20fe4b064675e6
Klaus Förster <klaus.foerster@uibk.ac.at>
2024-05-16 18:17:36 +0200

    add comments :-)

--- main.js
+++ main.js
@@ -113,5 +113,8 @@ async function loadWind(url) {
         (<a href="${url}" target="met.no">Stand ${forecastDate.toLocaleString()}</a>)
     `;
 
+    //document.querySelector("#map").innerHTML = "heute keine Karte";
+
+    //document.querySelector("body").innerHTML = "das war's für heute";
 }
 loadWind("https://geographie.uibk.ac.at/data/ecmwf/data/wind-10u-10v-europe.json");
