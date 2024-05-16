a55e0c42c6363656284a3d85bbf015a5a0308dee
Klaus Förster <klaus.foerster@uibk.ac.at>
2024-05-16 15:53:52 +0200

    mit Popup für Wetterwerte beginnen

--- main.js
+++ main.js
@@ -39,8 +39,19 @@ async function showForecast(url) {
     console.log(jsondata);
     L.geoJSON(jsondata, {
         pointToLayer: function(feature, latlng) {
+            let content = `
+                <ul>
+                    <li>Luftdruck (hPa): air_pressure_at_sea_level</li>
+                    <li>Lufttemperatur (°C): air_temperature</li>
+                    <li>Bewölkungsgrad (%): cloud_area_fraction</li>
+                    <li>Niederschlag (mm): precipitation_amount</li>
+                    <li>Luftfeuchtigkeit (%): relative_humidity</li>
+                    <li>Windrichtung (°): wind_from_direction</li>
+                    <li>Windgeschwindigkeit (km/h): wind_speed</li>
+                </ul>
+            `;
             L.popup(latlng, {
-                content: '<p>Hello world!<br />This is a nice popup.</p>'
+                content: content
             }).openOn(themaLayer.forecast);
         }
     }).addTo(themaLayer.forecast);
