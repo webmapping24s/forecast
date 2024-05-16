ce4832501ed2e088c9b434f07cb9422886b77d05
Klaus Förster <klaus.foerster@uibk.ac.at>
2024-05-16 17:12:27 +0200

    Wettervorhersage on Klick für jeden Punkt der Welt

--- main.js
+++ main.js
@@ -72,3 +72,10 @@ async function showForecast(url) {
     }).addTo(themaLayer.forecast);
 }
 showForecast("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=47.267222&lon=11.392778");
+
+// auf Kartenklick reagieren
+map.on("click", function(evt) {
+    //console.log(evt);
+    //console.log(evt.latlng.lat, evt.latlng.lng);
+    showForecast(`https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${evt.latlng.lat}&lon=${evt.latlng.lng}`);
+});
