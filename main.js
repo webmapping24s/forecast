b197b71c434f92b95c198bdc8936d48cebd61838
Klaus Förster <klaus.foerster@uibk.ac.at>
2024-05-16 16:05:46 +0200

    Niederschlag aus Popup entfernet weil nicht verfügbar

--- main.js
+++ main.js
@@ -45,7 +45,6 @@ async function showForecast(url) {
                     <li>Luftdruck (hPa): ${details.air_pressure_at_sea_level}</li>
                     <li>Lufttemperatur (°C): ${details.air_temperature}</li>
                     <li>Bewölkungsgrad (%): ${details.cloud_area_fraction}</li>
-                    <li>Niederschlag (mm): ${details.precipitation_amount}</li>
                     <li>Luftfeuchtigkeit (%): ${details.relative_humidity}</li>
                     <li>Windrichtung (°): ${details.wind_from_direction}</li>
                     <li>Windgeschwindigkeit (km/h): ${Math.round(details.wind_speed * 3.6)}</li>
