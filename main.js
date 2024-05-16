27503b6f6faef7147ec9d25b2a6c8e332eea8fe9
Klaus Förster <klaus.foerster@uibk.ac.at>
2024-05-16 08:46:15 +0200

    Template für Wind & Wetter Beispiel ausgepackt

---
+++ main.js
@@ -0,0 +1,41 @@
+/* Wind & Wetter Beispiel */
+
+// Innsbruck
+let ibk = {
+    lat: 47.267222,
+    lng: 11.392778
+};
+
+// Karte initialisieren
+let map = L.map("map", {
+    fullscreenControl: true
+}).setView([ibk.lat, ibk.lng], 5);
+
+// thematische Layer
+let themaLayer = {
+    forecast: L.featureGroup().addTo(map)
+}
+
+// Hintergrundlayer
+let layerControl = L.control.layers({
+    "Openstreetmap": L.tileLayer.provider("OpenStreetMap.Mapnik"),
+    "Esri WorldTopoMap": L.tileLayer.provider("Esri.WorldTopoMap"),
+    "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery").addTo(map)
+}, {
+    "Wettervorhersage MET Norway": themaLayer.forecast
+}).addTo(map);
+
+// Maßstab
+L.control.scale({
+    imperial: false,
+}).addTo(map);
+
+// Wettervorhersage MET Norway
+async function showForecast(url) {
+    let response = await fetch(url);
+    let jsondata = await response.json();
+
+    // aktuelles Wetter und Wettervorhersage implementieren
+    console.log(jsondata);
+}
+showForecast("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=47.267222&lon=11.392778");
